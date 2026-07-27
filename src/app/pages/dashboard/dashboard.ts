0e01a73b53725249a8ed384189ca787b59e7de6b
BryanAlexander-Reyes <brayanreyes47k@gmail.com>
2026-07-27 15:52:11 -0500

    se genera estructura html, y typeScript del dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -11,9 +11,94 @@ export class DashboardComponent {
   rol: string='Administrador del sistema'
 
 
+
+  // Variable que simula los datos del dashborad
+  estadisticas={
+    usuarios:120,
+    roles:8,
+    cursos:24,
+    prendices:560
+  };
+// json con arreglo
+  grafica=[
+    {
+      mes:'Enero',
+      valor:80,
+    },
+    {
+      mes:'Febrero',
+      valor:45,
+    },
+    {
+      mes:'Marzo',
+      valor:95,
+    },
+    {
+      mes:'Abril',
+      valor:70
+    },
+    {
+      mes:'Mayo',
+      valor:60,
+    },
+    {
+      mes:'Junio',
+      valor:88,
+    }
+  ];
+
+  usuarios=[
+    {
+      id:1,
+      nombre:'Juan Perez',
+      correo:'juanp@gmail.com',
+      rol:'Instructor',
+      estado:'activo'
+    },
+    {
+      id:2,
+      nombre:'Maria Gomez',
+      correo:'maria@sena.edu.co',
+      rol:'Aprendiz',
+      estado:'Activo'
+    },
+    {
+      id:3,
+      nombre:'Carlos Rodriguez',
+      correo:'carlosR@sena.edu.co',
+      rol:'Administrador',
+      estado:'Activo'
+    },
+    {
+      id:4,
+      nombre:'Laura Sanchez',
+      correo:'lauraS@sena.edu.co',
+      rol:'Instructor',
+      estado:'Inactiva'
+    },
+    {
+      id:5,
+      nombre:'Andres Torres',
+      correo:'Andres@sena.edu.co',
+      rol:'Aprendiz',
+      estado:'Activo'
+    }
+
+  ];
+
+  constructor(){
+    console.log("Dashborad cargado correctamente")
+  }
+
   // metodo para mostrar mensaje
 
   mostrarMensaje():void{
     alert('bienvenido al Dashboard del sistema');
+  }
+
+  // metodo nuevo para obtener todos los usuarios
+
+  obtenerTotalUsuarios():number{
+    return this.usuarios.length;
   }
 }
