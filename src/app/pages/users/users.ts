37fb3524c8c1bf6cb25d36bcc203fdebe7512f15
BryanAlexander-Reyes <brayanreyes47k@gmail.com>
2026-07-30 17:17:36 -0500

    se corrige codigo

--- src/app/pages/users/users.ts
+++ src/app/pages/users/users.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgForOf } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
+import { NgForOf, NgIf } from "@angular/common";
 
 interface Usuario{
   id:number;
@@ -12,7 +12,7 @@ interface Usuario{
 }
 @Component({
   selector: 'app-users',
-  imports: [FormsModule, NgForOf],
+  imports: [FormsModule, NgForOf, NgIf],
   templateUrl: './users.html',
   styleUrl: './users.css',
 })
@@ -28,6 +28,16 @@ export class UsersComponent implements OnInit {
   // variables de listas para usuarios, un arreglo de usuario
 
   usuarios:Usuario[]=[];
+
+  // mensajes de errores
+  mensajes:string=''
+  tipoMensaje:'success'  | 'error'  |  ''='';
+
+  // Bandera para saber si se esta editando un usuario
+
+  modoEdicion: boolean=false;
+
+  
 
   // linea que utiliza metodo para iniciar desde el principio
   ngOnInit(): void {
@@ -63,16 +73,54 @@ export class UsersComponent implements OnInit {
     ]
   }
 
+  // otra manera de validar 
+  validadorCorreo(correo:string): boolean{
+    const expresion=/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+[a-zA]{2,}$/;
+    return expresion.test(correo)
+  }
+
+
+  correoExiste(correo:string): boolean{
+    return this.usuarios.some(usuario=>usuario.correo.toLowerCase()===correo.toLowerCase())
+  }
   limpiarFormulario():void{
+
+    const respuesta= confirm('¿Desea limpiar el Formulario?')
+    if (!respuesta){
+      return
+    }
     this.id=0;
     this.nombre='';
     this.apellido='';
     this.correo='';
     this.rol='Aprendiz';
     this.estado=true;
+    this.mensajes='';
   }
 
   registrarUsuario():void{
+
+    // validacion de campos
+    if(this.nombre.trim()===''|| this.apellido.trim()===''||this.correo.trim()===''){
+      this.tipoMensaje='error';
+      this.mensajes='Todos los campos son obligatorios';
+      return
+    }
+
+    // validacion correo
+    if(!this.validadorCorreo(this.correo)){
+      this.tipoMensaje='error';
+      this.mensajes='El formato del correo es incorrecto';
+      return
+    }
+
+    // validar duplicados
+    if(this.correoExiste(this.correo)){
+      this.tipoMensaje='error';
+      this.mensajes='El correo ya se encuentra registrado.';
+      return
+    }
+
     // se construye un nuevo objeto usuario utilizando la informacion ingresada en el formulario
     const nuevoUsuario:Usuario={
       id:this.usuarios.length+1,
@@ -84,7 +132,20 @@ export class UsersComponent implements OnInit {
     };
     // agrega el nuevo objeto al arreglo
     this.usuarios.push(nuevoUsuario);
-    alert('Usuario registrado correctamente.')
-    this.limpiarFormulario();
+    this.tipoMensaje='success';
+    this.mensajes='Usuario registrados correctamente.';
+
+    this.limpiarFormularioAutomaticamente();
+  }
+  limpiarFormularioAutomaticamente():void{
+    this.id=0;
+    this.nombre='';
+    this.apellido='';
+    this.correo='';
+    this.rol='Aprendiz';
+    this.estado=true;
+  }
+  obtenerTotalUsuarios():number{
+    return this.usuarios.length;
   }
 }
